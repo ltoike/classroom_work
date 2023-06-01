0d5abf4f2ada2ab1e658685d0da3e01e7bbbcb4d
ltoike <latifa.toike@gmail.com>
2023-06-01 21:49:43 +0600

    sfsfs
    fsfs

---
+++ June 1/01.py
@@ -0,0 +1,27 @@
+#урок 5
+<html>
+<body>
+<style>
+    .b{
+        height: 100px;
+        width: 100px;
+        border: 1px solid red;
+        overflow: scroll;
+    }
+    .txt{
+        color: blue;
+    }
+    </style>
+    <span class="block line">
+    <div class="margin"
+
+
+    </div>
+    </div>
+    <div class="b">
+        <span> My fav things </span>
+
+    </div>
+
+</body>
+</html>
